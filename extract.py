985c29f2430b8ea5fc54ec227f436cc6fd0073f6
Ruchira Ramani <rramani@groupon.com>
2016-04-02 13:09:46 -0500

    added git pull functionality

--- extract.py
+++ extract.py
@@ -1,13 +1,12 @@
-#TODO: Need to delete the repo first before cloning it
+#TODO: Need to comment out the clone repo command after first run
 
 import git
 
-# Cloning Tracy's repo
+# Cloning Tracy's repo (One time clone)
 git.Git().clone("https://github.com/triketora/women-in-software-eng.git", "/women-in-tech-datasets/triketora")
 
-# Our cloned repo
-cloned_repo = git.Git("/Users/rramani/rramani/women-in-tech-datasets/dataSet")
+# Git pull (cron job)
+cloned_repo = git.cmd.Git("/women-in-tech-datasets/triketora")
 
 # Getting historic commit information for data.txt
 loginfo = cloned_repo.log('--', '-p', '--', 'data.txt')
-print loginfo
