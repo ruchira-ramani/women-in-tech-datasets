bc5ed2b2eb9a8633ab7e375a3df0001d3f09923c
Ruchira Ramani <rramani@groupon.com>
2016-04-02 12:57:24 -0500

    Cloning the repo and then getting specifics for data.txt

--- extract.py
+++ extract.py
@@ -1,7 +1,13 @@
-import pygithub3
-from pygithub3 import Github
-gh = Github()
+#TODO: Need to delete the repo first before cloning it
 
-# lists all the commits that reference data.txt tracy's repo
-data_hist = gh.repos.commits.list(user='triketora',repo='women-in-software-eng',sha='master',path='data.txt')
-print(data_hist.all())
+import git
+
+# Cloning Tracy's repo
+git.Git().clone("https://github.com/triketora/women-in-software-eng.git", "/women-in-tech-datasets/triketora")
+
+# Our cloned repo
+cloned_repo = git.Git("/Users/rramani/rramani/women-in-tech-datasets/dataSet")
+
+# Getting historic commit information for data.txt
+loginfo = cloned_repo.log('--', '-p', '--', 'data.txt')
+print loginfo
